3aa2150de84473fab73a5e0368524a0e475b278e
Venkata Penumatsa <penumatv@gmail.com>
2022-07-14 22:07:20 +0530

    Update MyEnrollmentComponent.js

--- components/Enroll/MyEnrollmentComponent.js
+++ components/Enroll/MyEnrollmentComponent.js
@@ -3,6 +3,7 @@ import useSWR from "swr";
 //Components
 import CircularLoading from "../Util/CircularLoading";
 import MyEnrollmentComponentData from "./MyEnrollmentComponentData";
+import { NextSeo } from "next-seo";
 
 const fetcher = async (...args) => {
   const response = await fetch(...args, {
@@ -23,6 +24,10 @@ const MyEnrollmentComponent = () => {
 
   return (
     <div>
+      <NextSeo
+        title="My Enrollments | Programmer Path"
+        description="Programmer Path is a learning management system (LMS) that will provide technical courses."
+      />
       <MyEnrollmentComponentData data={data.data} />
     </div>
   );
